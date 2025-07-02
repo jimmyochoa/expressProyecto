207a316d1160b31c737ec02afd607e0917536014
jimmyochoa <jimmyochoaholguin@gmail.com>
2025-07-01 19:05:37 -0500

    implement photos routes

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/photos', async function (req, res, next) {
+  const URL = 'https://dawm-fiec-espol-default-rtdb.firebaseio.com/photos.json';
+  
+  try {
+    const response = await axios.get(URL);
+    res.render('fotos', { title: 'Fotos', fotos: response.data });
+  } catch (error) {
+    console.error('Error fetching photos:', error.message);
+    res.status(500).send('Error fetching photos');
+  }
+});
+
 
 module.exports = router;
